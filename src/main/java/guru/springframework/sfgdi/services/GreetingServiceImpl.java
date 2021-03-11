f3444f66bdf3ef1544ce4f5079ceefeb0b00d9c6
Alexander Tscherwonnych <alex.tsch@gmail.com>
2021-03-11 19:56:05 +0100

    Manual DI. ConstructorInjection/PropertyInjection/SetterInjection

---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgdi.services;
+
+public class GreetingServiceImpl implements GreetingService {
+    public String sayGreeting() {
+        return "Hello World";
+    }
+}
