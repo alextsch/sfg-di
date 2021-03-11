ba83ffe1f665b0bc9e38434fbdc3c7784b119d1e
Alexander Tscherwonnych <alex.tsch@gmail.com>
2021-03-11 20:22:24 +0100

    Dependency Injection using Spring Framework

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,33 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
+import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		var ctx = SpringApplication.run(SfgDiApplication.class, args);
+		var myController = (MyController) ctx.getBean("myController");
+		var greeting = myController.sayHello();
+		System.out.println(greeting);
+
+		System.out.println("------ Property");
+		var propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("------ Setter");
+		var setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("------ Constructor");
+		var constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,4 +1,10 @@
 package guru.springframework.sfgdi.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
+    public String sayHello() {
+        return "Hello World";
+    }
 }
